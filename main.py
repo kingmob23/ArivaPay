906b4ceac29d7c44d17e3fc60407528e8208176c
yaroslav.safonov <yaroslav.safonov@latoken.com>
2023-09-28 17:48:35 +0300

    added admin page template

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from flask_admin import Admin
 
 app = Flask(__name__)
 
+# set optional bootswatch theme
+app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+
+admin = Admin(app, name='adminpage', template_mode='bootstrap3')
+# Add administrative views here
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>MAIN PAGE</p>"
