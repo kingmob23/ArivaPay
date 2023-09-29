0054cb813f45687ebfaef83c41a54fa0db1503e7
yaroslav.safonov <yaroslav.safonov@latoken.com>
2023-09-29 18:33:41 +0300

    vk auth bad try

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_admin import Admin
 
 app = Flask(__name__)
 
 # set optional bootswatch theme
-app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+app.config["FLASK_ADMIN_SWATCH"] = "cerulean"
 
-admin = Admin(app, name='adminpage', template_mode='bootstrap3')
+admin = Admin(app, name="adminpage", template_mode="bootstrap3")
 # Add administrative views here
+
 
 @app.route("/")
 def hello_world():
     return "<p>MAIN PAGE</p>"
+
+
+@app.route("/auth")
+def auth_page():
+    return render_template("auth.html")
