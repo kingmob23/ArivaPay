d9390d2d1d76e7d652478088d13c7eae512bee5e
yaroslav.safonov <yaroslav.safonov@latoken.com>
2023-10-25 19:55:26 +0300

    fix db initialization 8

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ db = SQLAlchemy()
 
 
 class User(db.Model):
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True, nullable=False)
 
