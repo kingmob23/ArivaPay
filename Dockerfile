2c236b2213c031a29c86510b5757ce337d9ab05b
yaroslav.safonov <yaroslav.safonov@latoken.com>
2023-11-02 18:53:03 +0300

    fix

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,4 @@ COPY . .
 
 EXPOSE 8000
 
-CMD ["gunicorn", "-w 4", "--bind", "0.0.0.0:8000", "app:app"]
+CMD ["gunicorn", "-w 1", "--bind", "0.0.0.0:8000", "app:app"]
--- models/db_init.py
+++ models/db_init.py
@@ -1,6 +1,6 @@
-from models import db
-
 def init_db(app):
+    from models import db
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    # db.init_app(app)
+    db.init_app(app)
+
     return db
