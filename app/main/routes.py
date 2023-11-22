3895db39561542a2010b7bad8f305bd89c3c894a
yaroslav.safonov <yaroslav.safonov@latoken.com>
2023-11-22 12:53:07 +0300

    import auth route to main routes

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,4 +1,6 @@
 from . import main
+from app.auth import auth_page
+
 
 @main.route("/")
 def index_page():
@@ -7,3 +9,7 @@ def index_page():
 @main.route("/account")
 def account_page():
     return "<p>Personal Account</p>"
+
+@main.route("/auth")
+def auth_page_proxy():
+    return auth_page()
